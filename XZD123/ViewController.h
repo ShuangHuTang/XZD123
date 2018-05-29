e6d1b230458de8390240589785244e990a7e973b
汤双湖 <shtang@192.168.0.101>
2018-05-29 17:02:59 +0800

    Initial Commit

---
+++ XZD123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZD123
+//
+//  Created by 汤双湖 on 2018/5/29.
+//  Copyright © 2018年 汤双湖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XZD123Tests/XZD123Tests.m
@@ -0,0 +1,39 @@
+//
+//  XZD123Tests.m
+//  XZD123Tests
+//
+//  Created by 汤双湖 on 2018/5/29.
+//  Copyright © 2018年 汤双湖. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XZD123Tests : XCTestCase
+
+@end
+
+@implementation XZD123Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
